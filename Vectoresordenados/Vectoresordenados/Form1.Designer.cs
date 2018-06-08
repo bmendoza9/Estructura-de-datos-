e5ce2fc9c6fd64c8da1704952357447d58356172
bmendoza9 <36267661+bmendoza9@users.noreply.github.com>
2018-06-07 20:13:11 -0500

    Add files via upload

---
+++ Vectoresordenados/Vectoresordenados/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace Vectoresordenados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBxNombre = new System.Windows.Forms.TextBox();
+            this.txtBxTelefono = new System.Windows.Forms.TextBox();
+            this.txtBxEmail = new System.Windows.Forms.TextBox();
+            this.txtBxEdad = new System.Windows.Forms.TextBox();
+            this.txtBxAPaterno = new System.Windows.Forms.TextBox();
+            this.txtBxAMaterno = new System.Windows.Forms.TextBox();
+            this.txtBxTelBuscar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button_agregar = new System.Windows.Forms.Button();
+            this.button_buscar = new System.Windows.Forms.Button();
+            this.button_eliminar = new System.Windows.Forms.Button();
+            this.button_listar = new System.Windows.Forms.Button();
+            this.txtBxTabla = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtBxNombre
+            // 
+            this.txtBxNombre.Location = new System.Drawing.Point(73, 12);
+            this.txtBxNombre.Name = "txtBxNombre";
+            this.txtBxNombre.Size = new System.Drawing.Size(81, 20);
+            this.txtBxNombre.TabIndex = 0;
+            // 
+            // txtBxTelefono
+            // 
+            this.txtBxTelefono.Location = new System.Drawing.Point(73, 38);
+            this.txtBxTelefono.Name = "txtBxTelefono";
+            this.txtBxTelefono.Size = new System.Drawing.Size(81, 20);
+            this.txtBxTelefono.TabIndex = 1;
+            // 
+            // txtBxEmail
+            // 
+            this.txtBxEmail.Location = new System.Drawing.Point(73, 64);
+            this.txtBxEmail.Name = "txtBxEmail";
+            this.txtBxEmail.Size = new System.Drawing.Size(81, 20);
+            this.txtBxEmail.TabIndex = 2;
+            // 
+            // txtBxEdad
+            // 
+            this.txtBxEdad.Location = new System.Drawing.Point(73, 90);
+            this.txtBxEdad.Name = "txtBxEdad";
+            this.txtBxEdad.Size = new System.Drawing.Size(81, 20);
+            this.txtBxEdad.TabIndex = 3;
+            // 
+            // txtBxAPaterno
+            // 
+            this.txtBxAPaterno.Location = new System.Drawing.Point(73, 116);
+            this.txtBxAPaterno.Name = "txtBxAPaterno";
+            this.txtBxAPaterno.Size = new System.Drawing.Size(81, 20);
+            this.txtBxAPaterno.TabIndex = 4;
+            // 
+            // txtBxAMaterno
+            // 
+            this.txtBxAMaterno.Location = new System.Drawing.Point(73, 142);
+            this.txtBxAMaterno.Name = "txtBxAMaterno";
+            this.txtBxAMaterno.Size = new System.Drawing.Size(81, 20);
+            this.txtBxAMaterno.TabIndex = 5;
+            // 
+            // txtBxTelBuscar
+            // 
+            this.txtBxTelBuscar.Location = new System.Drawing.Point(183, 35);
+            this.txtBxTelBuscar.Name = "txtBxTelBuscar";
+            this.txtBxTelBuscar.Size = new System.Drawing.Size(81, 20);
+            this.txtBxTelBuscar.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Tel";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Edad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "A.Paterno";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 149);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "A.Materno";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(201, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(40, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Buscar";
+            // 
+            // button_agregar
+            // 
+            this.button_agregar.Location = new System.Drawing.Point(15, 180);
+            this.button_agregar.Name = "button_agregar";
+            this.button_agregar.Size = new System.Drawing.Size(75, 23);
+            this.button_agregar.TabIndex = 14;
+            this.button_agregar.Text = "Agregar";
+            this.button_agregar.UseVisualStyleBackColor = true;
+            this.button_agregar.Click += new System.EventHandler(this.button_agregar_Click);
+            // 
+            // button_buscar
+            // 
+            this.button_buscar.Location = new System.Drawing.Point(189, 61);
+            this.button_buscar.Name = "button_buscar";
+            this.button_buscar.Size = new System.Drawing.Size(75, 23);
+            this.button_buscar.TabIndex = 15;
+            this.button_buscar.Text = "Buscar";
+            this.button_buscar.UseVisualStyleBackColor = true;
+            this.button_buscar.Click += new System.EventHandler(this.button_buscar_Click);
+            // 
+            // button_eliminar
+            // 
+            this.button_eliminar.Location = new System.Drawing.Point(96, 180);
+            this.button_eliminar.Name = "button_eliminar";
+            this.button_eliminar.Size = new System.Drawing.Size(75, 23);
+            this.button_eliminar.TabIndex = 16;
+            this.button_eliminar.Text = "Eliminar";
+            this.button_eliminar.UseVisualStyleBackColor = true;
+            this.button_eliminar.Click += new System.EventHandler(this.button_eliminar_Click);
+            // 
+            // button_listar
+            // 
+            this.button_listar.Location = new System.Drawing.Point(189, 123);
+            this.button_listar.Name = "button_listar";
+            this.button_listar.Size = new System.Drawing.Size(75, 23);
+            this.button_listar.TabIndex = 17;
+            this.button_listar.Text = "Listar";
+            this.button_listar.UseVisualStyleBackColor = true;
+            this.button_listar.Click += new System.EventHandler(this.button_listar_Click);
+            // 
+            // txtBxTabla
+            // 
+            this.txtBxTabla.Location = new System.Drawing.Point(12, 222);
+            this.txtBxTabla.Multiline = true;
+            this.txtBxTabla.Name = "txtBxTabla";
+            this.txtBxTabla.Size = new System.Drawing.Size(423, 293);
+            this.txtBxTabla.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 538);
+            this.Controls.Add(this.txtBxTabla);
+            this.Controls.Add(this.button_listar);
+            this.Controls.Add(this.button_eliminar);
+            this.Controls.Add(this.button_buscar);
+            this.Controls.Add(this.button_agregar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBxTelBuscar);
+            this.Controls.Add(this.txtBxAMaterno);
+            this.Controls.Add(this.txtBxAPaterno);
+            this.Controls.Add(this.txtBxEdad);
+            this.Controls.Add(this.txtBxEmail);
+            this.Controls.Add(this.txtBxTelefono);
+            this.Controls.Add(this.txtBxNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBxNombre;
+        private System.Windows.Forms.TextBox txtBxTelefono;
+        private System.Windows.Forms.TextBox txtBxEmail;
+        private System.Windows.Forms.TextBox txtBxEdad;
+        private System.Windows.Forms.TextBox txtBxAPaterno;
+        private System.Windows.Forms.TextBox txtBxAMaterno;
+        private System.Windows.Forms.TextBox txtBxTelBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button_agregar;
+        private System.Windows.Forms.Button button_buscar;
+        private System.Windows.Forms.Button button_eliminar;
+        private System.Windows.Forms.Button button_listar;
+        private System.Windows.Forms.TextBox txtBxTabla;
+    }
+}
+
